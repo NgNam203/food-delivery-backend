dff9a0925fb24b568b9b23d667b633bdc06920c8
Nam Nguyen <nguyenhainam203@gmail.com>
2026-06-22 21:28:18 +0700

    feat(config): add environment configuration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private prisma: PrismaService) {}
-  async getHello() {
-    return this.prisma.user.count();
+  constructor(
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
+  getHello(): string {
+    return this.configService.get<string>('PORT')!;
   }
 }
